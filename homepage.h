d082c0efc95e3964b75418d0e218b962399562c6
ABDULLAh24k0612 <fahadkamran2103@gmail.com>
2025-04-26 05:20:32 -0700

    Update homepage.h

--- homepage.h
+++ homepage.h
@@ -1,1 +1,27 @@
+#ifndef HOMEPAGE_H
+#define HOMEPAGE_H
+
+#include <QWidget>
+#include <QSlider>
+#include <QPushButton>
+#include <QLabel>
+#include <QVBoxLayout>
+
+class HomePage : public QWidget {
+    Q_OBJECT
+
+public:
+    explicit HomePage(QWidget *parent = nullptr);
+
+    QSlider *getSlider() const;
+    QPushButton *getStartButton() const;
+    QLabel *getPlayerCountLabel() const;
+
+private:
+    QSlider *playerSlider;
+    QPushButton *startButton;
+    QLabel *playerCountLabel;
+};
+
+#endif // HOMEPAGE_H
 
