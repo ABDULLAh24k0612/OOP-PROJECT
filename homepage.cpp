2a02e7d287103d4f3b8b6f8343828a2a991798cc
ABDULLAh24k0612 <fahadkamran2103@gmail.com>
2025-04-26 05:20:52 -0700

    Update homepage.cpp

--- homepage.cpp
+++ homepage.cpp
@@ -1,1 +1,38 @@
+#include "homepage.h"
+
+HomePage::HomePage(QWidget *parent) : QWidget(parent) {
+    QVBoxLayout *layout = new QVBoxLayout(this);
+    layout->setContentsMargins(50, 100, 50, 50);
+    layout->setSpacing(30);
+
+    QLabel *title = new QLabel("Snake and Ladder");
+    title->setAlignment(Qt::AlignCenter);
+    title->setStyleSheet("font-size: 24px; font-weight: bold; color: white;");
+
+    playerSlider = new QSlider(Qt::Horizontal);
+    playerSlider->setRange(2, 4);
+    playerSlider->setValue(2);
+
+    playerCountLabel = new QLabel("Players: 2");
+    playerCountLabel->setStyleSheet("color: white;");
+
+    startButton = new QPushButton("Start Game");
+
+    layout->addWidget(title);
+    layout->addWidget(playerSlider);
+    layout->addWidget(playerCountLabel);
+    layout->addWidget(startButton);
+}
+
+QSlider* HomePage::getSlider() const {
+    return playerSlider;
+}
+
+QPushButton* HomePage::getStartButton() const {
+    return startButton;
+}
+
+QLabel* HomePage::getPlayerCountLabel() const {
+    return playerCountLabel;
+}
 
